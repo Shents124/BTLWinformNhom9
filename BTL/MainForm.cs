893e6d0133c24fe65a7c4bf981ccac542ab96e2d
Wolf <Wolf@DESKTOP-E7KV63R>
2021-08-04 18:34:28 +0700

    them Form Quan ly don nhap hang

--- BTL/MainForm.cs
+++ BTL/MainForm.cs
@@ -90,6 +90,7 @@ namespace BTL
         private void btnQLDonNhapHang_Click(object sender, EventArgs e)
         {
             // Hiển thị form quản lý đơn nhập hàng
+            OpenChildForm(new FormQuanLyDonHang());
             HideSubMenu();
         }
 
