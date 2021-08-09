0baa379b43e766d039643678696e5763c49e1a0a
Wolf <Wolf@DESKTOP-E7KV63R>
2021-08-09 11:59:25 +0700

    update formQuanLyDonHang_ThemButton

--- BTL/FormQuanLyDonHang.Designer.cs
+++ BTL/FormQuanLyDonHang.Designer.cs
@@ -93,7 +93,7 @@ namespace BTL
             this.panel1.Controls.Add(this.groupBox1);
             this.panel1.Location = new System.Drawing.Point(0, 36);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(1322, 748);
+            this.panel1.Size = new System.Drawing.Size(1322, 680);
             this.panel1.TabIndex = 2;
             // 
             // groupBox3
@@ -105,9 +105,9 @@ namespace BTL
             this.groupBox3.Controls.Add(this.panel3);
             this.groupBox3.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
             this.groupBox3.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(224)))), ((int)(((byte)(192)))));
-            this.groupBox3.Location = new System.Drawing.Point(3, 478);
+            this.groupBox3.Location = new System.Drawing.Point(3, 451);
             this.groupBox3.Name = "groupBox3";
-            this.groupBox3.Size = new System.Drawing.Size(1316, 267);
+            this.groupBox3.Size = new System.Drawing.Size(1316, 238);
             this.groupBox3.TabIndex = 2;
             this.groupBox3.TabStop = false;
             this.groupBox3.Text = "Chức năng";
@@ -123,7 +123,7 @@ namespace BTL
             this.panel4.Controls.Add(this.btnXem);
             this.panel4.Location = new System.Drawing.Point(556, 28);
             this.panel4.Name = "panel4";
-            this.panel4.Size = new System.Drawing.Size(751, 233);
+            this.panel4.Size = new System.Drawing.Size(751, 198);
             this.panel4.TabIndex = 16;
             // 
             // btnXoa
@@ -131,7 +131,7 @@ namespace BTL
             this.btnXoa.Anchor = System.Windows.Forms.AnchorStyles.None;
             this.btnXoa.Cursor = System.Windows.Forms.Cursors.Hand;
             this.btnXoa.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.btnXoa.Location = new System.Drawing.Point(456, 137);
+            this.btnXoa.Location = new System.Drawing.Point(456, 98);
             this.btnXoa.Name = "btnXoa";
             this.btnXoa.Size = new System.Drawing.Size(90, 90);
             this.btnXoa.TabIndex = 3;
@@ -143,7 +143,7 @@ namespace BTL
             this.btnSua.Anchor = System.Windows.Forms.AnchorStyles.None;
             this.btnSua.Cursor = System.Windows.Forms.Cursors.Hand;
             this.btnSua.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.btnSua.Location = new System.Drawing.Point(214, 137);
+            this.btnSua.Location = new System.Drawing.Point(214, 98);
             this.btnSua.Name = "btnSua";
             this.btnSua.Size = new System.Drawing.Size(90, 90);
             this.btnSua.TabIndex = 2;
@@ -155,7 +155,7 @@ namespace BTL
             this.btnThem.Anchor = System.Windows.Forms.AnchorStyles.None;
             this.btnThem.Cursor = System.Windows.Forms.Cursors.Hand;
             this.btnThem.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.btnThem.Location = new System.Drawing.Point(456, 2);
+            this.btnThem.Location = new System.Drawing.Point(456, 7);
             this.btnThem.Name = "btnThem";
             this.btnThem.Size = new System.Drawing.Size(90, 90);
             this.btnThem.TabIndex = 1;
@@ -167,12 +167,13 @@ namespace BTL
             this.btnXem.Anchor = System.Windows.Forms.AnchorStyles.None;
             this.btnXem.Cursor = System.Windows.Forms.Cursors.Hand;
             this.btnXem.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.btnXem.Location = new System.Drawing.Point(214, 2);
+            this.btnXem.Location = new System.Drawing.Point(214, 7);
             this.btnXem.Name = "btnXem";
             this.btnXem.Size = new System.Drawing.Size(90, 90);
             this.btnXem.TabIndex = 0;
             this.btnXem.Text = "Xem";
             this.btnXem.UseVisualStyleBackColor = true;
+            this.btnXem.Click += new System.EventHandler(this.btnXem_Click);
             // 
             // panel3
             // 
@@ -188,9 +189,9 @@ namespace BTL
             this.panel3.Controls.Add(this.label8);
             this.panel3.Controls.Add(this.label7);
             this.panel3.Controls.Add(this.label6);
-            this.panel3.Location = new System.Drawing.Point(6, 28);
+            this.panel3.Location = new System.Drawing.Point(9, 28);
             this.panel3.Name = "panel3";
-            this.panel3.Size = new System.Drawing.Size(544, 233);
+            this.panel3.Size = new System.Drawing.Size(544, 198);
             this.panel3.TabIndex = 0;
             // 
             // btnRefresh
@@ -199,7 +200,7 @@ namespace BTL
             this.btnRefresh.BackColor = System.Drawing.Color.Transparent;
             this.btnRefresh.Cursor = System.Windows.Forms.Cursors.Hand;
             this.btnRefresh.Image = global::BTL.Properties.Resources.icons8_refresh_48;
-            this.btnRefresh.Location = new System.Drawing.Point(451, 133);
+            this.btnRefresh.Location = new System.Drawing.Point(451, 92);
             this.btnRefresh.Name = "btnRefresh";
             this.btnRefresh.Size = new System.Drawing.Size(77, 78);
             this.btnRefresh.TabIndex = 26;
@@ -214,7 +215,7 @@ namespace BTL
             this.btnFilter.BackColor = System.Drawing.Color.Transparent;
             this.btnFilter.Cursor = System.Windows.Forms.Cursors.Hand;
             this.btnFilter.Image = global::BTL.Properties.Resources.icons8_filter_48;
-            this.btnFilter.Location = new System.Drawing.Point(405, 133);
+            this.btnFilter.Location = new System.Drawing.Point(405, 92);
             this.btnFilter.Name = "btnFilter";
             this.btnFilter.Size = new System.Drawing.Size(40, 78);
             this.btnFilter.TabIndex = 25;
@@ -229,7 +230,7 @@ namespace BTL
             this.btnSearch.BackColor = System.Drawing.Color.Transparent;
             this.btnSearch.Cursor = System.Windows.Forms.Cursors.Hand;
             this.btnSearch.Image = global::BTL.Properties.Resources.icons8_google_web_search_48;
-            this.btnSearch.Location = new System.Drawing.Point(405, 23);
+            this.btnSearch.Location = new System.Drawing.Point(405, 11);
             this.btnSearch.Name = "btnSearch";
             this.btnSearch.Size = new System.Drawing.Size(40, 40);
             this.btnSearch.TabIndex = 24;
@@ -241,7 +242,7 @@ namespace BTL
             this.dtpKT.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
             | System.Windows.Forms.AnchorStyles.Left)));
             this.dtpKT.Format = System.Windows.Forms.DateTimePickerFormat.Custom;
-            this.dtpKT.Location = new System.Drawing.Point(177, 184);
+            this.dtpKT.Location = new System.Drawing.Point(177, 141);
             this.dtpKT.Name = "dtpKT";
             this.dtpKT.Size = new System.Drawing.Size(200, 29);
             this.dtpKT.TabIndex = 23;
@@ -251,7 +252,7 @@ namespace BTL
             this.dtpBD.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
             | System.Windows.Forms.AnchorStyles.Left)));
             this.dtpBD.Format = System.Windows.Forms.DateTimePickerFormat.Custom;
-            this.dtpBD.Location = new System.Drawing.Point(177, 133);
+            this.dtpBD.Location = new System.Drawing.Point(177, 90);
             this.dtpBD.Name = "dtpBD";
             this.dtpBD.Size = new System.Drawing.Size(200, 29);
             this.dtpBD.TabIndex = 22;
@@ -260,7 +261,7 @@ namespace BTL
             // 
             this.txtTimPhieu.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
             | System.Windows.Forms.AnchorStyles.Left)));
-            this.txtTimPhieu.Location = new System.Drawing.Point(115, 34);
+            this.txtTimPhieu.Location = new System.Drawing.Point(115, 22);
             this.txtTimPhieu.Name = "txtTimPhieu";
             this.txtTimPhieu.Size = new System.Drawing.Size(262, 29);
             this.txtTimPhieu.TabIndex = 21;
@@ -270,7 +271,7 @@ namespace BTL
             this.label9.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
             | System.Windows.Forms.AnchorStyles.Left)));
             this.label9.AutoSize = true;
-            this.label9.Location = new System.Drawing.Point(33, 190);
+            this.label9.Location = new System.Drawing.Point(33, 147);
             this.label9.Name = "label9";
             this.label9.Size = new System.Drawing.Size(106, 21);
             this.label9.TabIndex = 20;
@@ -281,7 +282,7 @@ namespace BTL
             this.label8.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
             | System.Windows.Forms.AnchorStyles.Left)));
             this.label8.AutoSize = true;
-            this.label8.Location = new System.Drawing.Point(33, 139);
+            this.label8.Location = new System.Drawing.Point(33, 96);
             this.label8.Name = "label8";
             this.label8.Size = new System.Drawing.Size(103, 21);
             this.label8.TabIndex = 19;
@@ -292,7 +293,7 @@ namespace BTL
             this.label7.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
             | System.Windows.Forms.AnchorStyles.Left)));
             this.label7.AutoSize = true;
-            this.label7.Location = new System.Drawing.Point(3, 86);
+            this.label7.Location = new System.Drawing.Point(3, 59);
             this.label7.Name = "label7";
             this.label7.Size = new System.Drawing.Size(150, 21);
             this.label7.TabIndex = 18;
@@ -303,7 +304,7 @@ namespace BTL
             this.label6.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
             | System.Windows.Forms.AnchorStyles.Left)));
             this.label6.AutoSize = true;
-            this.label6.Location = new System.Drawing.Point(0, 37);
+            this.label6.Location = new System.Drawing.Point(0, 25);
             this.label6.Name = "label6";
             this.label6.Size = new System.Drawing.Size(79, 21);
             this.label6.TabIndex = 17;
@@ -317,9 +318,9 @@ namespace BTL
             this.groupBox2.Controls.Add(this.dgvCT);
             this.groupBox2.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
             this.groupBox2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(224)))), ((int)(((byte)(192)))));
-            this.groupBox2.Location = new System.Drawing.Point(3, 232);
+            this.groupBox2.Location = new System.Drawing.Point(3, 205);
             this.groupBox2.Name = "groupBox2";
-            this.groupBox2.Size = new System.Drawing.Size(1316, 239);
+            this.groupBox2.Size = new System.Drawing.Size(1316, 240);
             this.groupBox2.TabIndex = 1;
             this.groupBox2.TabStop = false;
             this.groupBox2.Text = "Chi tiết phiếu nhập";
@@ -340,7 +341,7 @@ namespace BTL
             this.panel2.Controls.Add(this.txtMaPhieu);
             this.panel2.Location = new System.Drawing.Point(9, 28);
             this.panel2.Name = "panel2";
-            this.panel2.Size = new System.Drawing.Size(367, 185);
+            this.panel2.Size = new System.Drawing.Size(446, 195);
             this.panel2.TabIndex = 17;
             // 
             // label2
@@ -422,10 +423,10 @@ namespace BTL
             | System.Windows.Forms.AnchorStyles.Right)));
             this.dgvCT.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
             this.dgvCT.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dgvCT.Location = new System.Drawing.Point(381, 28);
+            this.dgvCT.Location = new System.Drawing.Point(461, 28);
             this.dgvCT.Name = "dgvCT";
             this.dgvCT.RowTemplate.Height = 25;
-            this.dgvCT.Size = new System.Drawing.Size(934, 185);
+            this.dgvCT.Size = new System.Drawing.Size(882, 195);
             this.dgvCT.TabIndex = 0;
             // 
             // groupBox1
@@ -438,7 +439,7 @@ namespace BTL
             this.groupBox1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(224)))), ((int)(((byte)(192)))));
             this.groupBox1.Location = new System.Drawing.Point(3, 3);
             this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(1316, 222);
+            this.groupBox1.Size = new System.Drawing.Size(1316, 196);
             this.groupBox1.TabIndex = 0;
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "Danh sách phiếu nhập";
@@ -449,10 +450,10 @@ namespace BTL
             | System.Windows.Forms.AnchorStyles.Right)));
             this.dgvDS.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
             this.dgvDS.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dgvDS.Location = new System.Drawing.Point(0, 28);
+            this.dgvDS.Location = new System.Drawing.Point(6, 28);
             this.dgvDS.Name = "dgvDS";
             this.dgvDS.RowTemplate.Height = 25;
-            this.dgvDS.Size = new System.Drawing.Size(1316, 180);
+            this.dgvDS.Size = new System.Drawing.Size(1304, 153);
             this.dgvDS.TabIndex = 0;
             // 
             // FormQuanLyDonHang
@@ -460,11 +461,12 @@ namespace BTL
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(20)))), ((int)(((byte)(30)))), ((int)(((byte)(40)))));
-            this.ClientSize = new System.Drawing.Size(1322, 853);
+            this.ClientSize = new System.Drawing.Size(1322, 728);
             this.Controls.Add(this.panel1);
             this.Controls.Add(this.button1);
             this.Name = "FormQuanLyDonHang";
             this.Text = "FormQuanLyDonHang";
+            this.Load += new System.EventHandler(this.FormQuanLyDonHang_Load);
             this.panel1.ResumeLayout(false);
             this.groupBox3.ResumeLayout(false);
             this.panel4.ResumeLayout(false);
