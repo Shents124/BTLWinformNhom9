99f5250136c169c13f6468fcb7ae122ee1d4bc34
Wolf <Wolf@DESKTOP-E7KV63R>
2021-08-28 16:15:02 +0700

    update Giao dien, fix loi bai lam Phuc

--- BTL/Models/QLBanSachContext.cs
+++ BTL/Models/QLBanSachContext.cs
@@ -32,7 +32,7 @@ namespace BTL.Models
             if (!optionsBuilder.IsConfigured)
             {
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
-                optionsBuilder.UseSqlServer("Data Source=ACER-A7\\SQLEXPRESS;Initial Catalog=QLBanSach;Integrated Security=True");
+                optionsBuilder.UseSqlServer("Data Source=DESKTOP-E7KV63R;Initial Catalog=QLBanSach;Integrated Security=True");
             }
         }
 
